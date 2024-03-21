2ce0435f9f4fa477ecce90a77b66d87eab96aa37
Taha Ergenoğlu <tahaergenoglu@gmail.com>
2024-03-21 15:01:42 +0300

    Proje dosyası ekle.

---
+++ Kitabı Bitirme Süres/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitabı_Bitirme_Süres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kitabın sayfa sayısı verilecek
+            // ilk gün okunan kitap sayısı verilecek
+            // sonraki günlerde okunan sayfa sayısı %10 artacak
+
+            float sayfa = Convert.ToInt32(Console.ReadLine());
+            float okunan=Convert.ToInt32(Console.ReadLine());
+            int gün = 0;
+            float toplam = Convert.ToInt32(Console.ReadLine());
+            if (sayfa <= toplam)
+                okunan = okunan + (okunan * 1/10);
+            toplam += okunan;
+            gün++;
+            Console.WriteLine($"gün={gün}");
+
+
+
+        }
+    }
+}
